5e39675be90184712b4fcf8782224eec8be11c7a
m0ntz <109100764+m0ntz@users.noreply.github.com>
2023-01-06 20:13:32 +0100

    added start and chat screen

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from "expo-status-bar";
+import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// Import screens
+import Start from "./components/Start";
+import Chat from "./components/Chat";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+import "react-native-gesture-handler";
+
+// React Navigation
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+// Create the navigator
+const Stack = createStackNavigator();
+
+export default class App extends Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Start">
+          <Stack.Screen name="Start" component={Start} />
+          <Stack.Screen name="Chat" component={Chat} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
