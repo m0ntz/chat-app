1400612c682d23300b2cd6305c04cf86f49063b3
m0ntz <109100764+m0ntz@users.noreply.github.com>
2023-01-07 14:09:49 +0100

    implemented Gifted Chat

--- components/Chat.js
+++ components/Chat.js
@@ -1,30 +1,85 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet, Platform, KeyboardAvoidingView } from "react-native";
+import { GiftedChat, Bubble } from "react-native-gifted-chat";
 
 export default class Chat extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    };
+  }
+
   componentDidMount() {
+    // Show Username in Chat title
     let name = this.props.route.params.name;
     this.props.navigation.setOptions({ title: name });
+
+    // Show messages
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: `Hi, ${name} 👋`,
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "React Native",
+            avatar: "https://placeimg.com/140/140/any",
+          },
+        },
+        {
+          _id: 2,
+          text: `${name} has entered the chat`,
+          createdAt: new Date(),
+          system: true,
+        },
+      ],
+    });
   }
+
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
+
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: "#e07a5f",
+          },
+        }}
+      />
+    );
+  }
+
   render() {
     let color = this.props.route.params.color;
     return (
       <View style={[styles.container, { backgroundColor: color }]}>
-        <Text style={styles.welcomeText}>Entering Chat...</Text>
+        <GiftedChat
+          renderBubble={this.renderBubble.bind(this)}
+          messages={this.state.messages}
+          onSend={(messages) => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+        />
+        {Platform.OS === "android" ? (
+          <KeyboardAvoidingView behavior="height" />
+        ) : null}
       </View>
     );
   }
 }
 
+//Styling
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-
-  welcomeText: {
-    color: "#fff",
-    fontSize: "17px",
   },
 });
